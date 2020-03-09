2fbf89d44bb364d7c5848e1a69dd0bd7b131aacd
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 23:15:20 -0500

    tested with bigger bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,6 +99,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
+
+        #Check for any bullets that have hit aliens.
+        #If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
             
     def _update_aliens(self):
         #Check if the fleet is at an edge,then update the positions of all aliens in the fleet.
