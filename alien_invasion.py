40c6ee2f3e5d2003acc009fa577a2f38fa34a902
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 21:08:53 -0500

    added settings file and changes settings in alien file

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+
 class AlienInvasion:
     #Overall class to manage game assets and behavior.
     def __init__(self):
         #Initialize the game, and create game resources.
         pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -21,6 +29,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
 #Make the most recently drawn screen visible.
 
