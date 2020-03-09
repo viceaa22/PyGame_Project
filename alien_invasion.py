31490694dd84b830c93afc58fcc7cc33691e73f8
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 22:13:23 -0500

    changed code to look organized

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,18 +36,20 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+            self._update_screen()
 
     def _update_bullets(self):
         #Update position of bullets and get rid of old bullets.
         #Update bullet positions.
         self.bullets.update()
-        
+
             #Get rid of bullets that have disappeared.
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
             print(len(self.bullets))
-            self._update_screen()
+            
 
             #Watch for keyboard and mouse events.
     def _check_events(self):
