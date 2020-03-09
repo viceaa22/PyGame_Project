c014300146decc36e75f8af31ae18eb2c4d1b124
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 21:23:23 -0500

    added ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -18,6 +19,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -31,6 +34,7 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
 #Make the most recently drawn screen visible.
 
