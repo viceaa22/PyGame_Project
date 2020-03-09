accd2790bfc8e748d6e8425f775e653e4299245b
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-09 01:06:24 -0500

    printed screboard with increasing values

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,8 +31,9 @@ class AlienInvasion:
         
         pygame.display.set_caption("Alien Invasion")
 
-        #Create an instance to store game statistics.
+        #Create an instance to store game statistics and create a scoreboard..
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -125,6 +126,11 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+
         if not self.aliens:
             #Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -199,6 +205,7 @@ class AlienInvasion:
             
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             #Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -247,6 +254,9 @@ class AlienInvasion:
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
             self.aliens.draw(self.screen)
+
+            #Draw the score information.
+            self.sb.show_score()
 
             #Draw the play button if the game is inactive.
             if not self.stats.game_active:
--- settings.py
+++ settings.py
@@ -25,6 +25,9 @@ class Settings:
         self.speedup_scale = 1.1
         self.initialize_dynamic_settings()
 
+        #How quickly the alien point values increase
+        self.score_scale = 1.5
+
 
         #Screen settings
         self.screen_width = 1200
@@ -38,9 +41,16 @@ class Settings:
         self.alien_speed = 1.0
         #fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        #Scoring
+        self.alien_points = 50
     
     def increase_speed(self):
-        #Increase speed settings.
+        #Increase speed settings and alien point values.
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
+        print(self.alien_points)
