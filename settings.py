0558c93f32874a62416f5e7971ecb3bb5a13dfb6
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 23:17:24 -0500

    repopulated fleet and incresed bullet speed

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
      #Initialize the game's settings.
 
      #Bullet settings
-     self.bullet_speed = 1.0
+     self.bullet_speed = 1.5
      self.bullet_width = 3
      self.bullet_height = 15
      self.bullet_color = (60, 60, 60)
