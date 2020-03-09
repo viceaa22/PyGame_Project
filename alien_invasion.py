71b1c429229addbf1fc26747337ca2df2430d024
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 23:54:03 -0500

    added button to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 class AlienInvasion:
     #Overall class to manage game assets and behavior.
@@ -37,6 +38,10 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
         
         self._create_fleet()
+        
+        #Make the Play button.
+        self.play_button = Button(self, "Play")
+
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -215,6 +220,10 @@ class AlienInvasion:
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
             self.aliens.draw(self.screen)
+
+            #Draw the play button if the game is inactive.
+            if not self.stats.game_active:
+                self.play_button.draw_button()
 
 #Make the most recently drawn screen visible.
 
