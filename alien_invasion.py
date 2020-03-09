6d9f86537dfb6fa7c89bad7962f12cee241f44b3
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 23:21:40 -0500

    added shit hit message and stats file

--- alien_invasion.py
+++ alien_invasion.py
@@ -122,6 +122,10 @@ class AlienInvasion:
         #Check if the fleet is at an edge,then update the positions of all aliens in the fleet.
         self._check_fleet_edges()
         self.aliens.update()
+
+        #Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
             
     def _check_events(self):
         #Respond to keypresses and mouse events.
