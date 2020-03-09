3e52da25102d713d8c3470a686e128764a442040
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-03-08 23:47:04 -0500

    run active parts

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,10 +45,13 @@ class AlienInvasion:
         #Start the main loop for the game.
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
-            self._update_screen()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
+        self._update_screen()
     
     def _create_fleet(self):
         #Create the fleet of aliens.
